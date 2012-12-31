75b09e5d27ef8f5e6e2537a38922f1a6a8e5167a
syntax@arbitraryy.com <syntax@arbitraryy.com>
2012-12-31 12:12:13 -0800

    Updated to version from laptop

--- perlOSC/osc_test/test.pl
+++ perlOSC/osc_test/test.pl
@@ -3,15 +3,41 @@
 use lib 'C:\xampp\perl\lib';
 use Net::OpenSoundControl::Client; 
 
-print "Sending msg '/Main/Volume', 'f', $_ / 100";
-my $port = "9999";
-my $host = "192.168.1.177";
+my $port = '';
+
+if (@ARGV != 1) {
+	print "\n\tusage: perl sendOSC.pl <port#>\n\n";
+	exit;
+} else {
+	$port = $ARGV[0];
+}
 my $client = Net::OpenSoundControl::Client->new(
-		Host => $host, Port => $port)
+		Host => "127.0.0.1", Port => $port)
 		or die "could not start client: $@\n";
 
+#connect to PDE OSC Server
+#$client->send(['/server/connect']);
+
+#Send test OSC messages
+$color = 'red';
 for (0..100) {
-	print "Sending msg '/Main/Volume', 'f', $_ / 100";
-	$client->send(['/Main/Volume', 'f', $_ / 100]);
-	sleep(1);	
+	$OSC_color = '';
+	#$client->send(['/layer1/pos_x', 'f', $_ / 30]);
+	#$client->send(['/layer1/fade', 'f', $_ /30]);
+	if ($color eq 'red') {
+		$color = 'blue';
+	    } elsif ($color eq 'blue') {
+		$color = 'green';
+	    } else {
+		$color = 'red';
+	    }
+	    $mesg = "/layer1/pos_x";
+	    $mesg2 = "/layer1/pos_y";
+	$client->send(["$mesg", 'f', $_ / 150]);
+	$client->send(["$mesg2", 'f', $_ / 150]);
+	#print "/Main/Volume f $_ / 100 ";
+	print "message $_; Message is $mesg($_ / 10),$mesg2($_ / 30 ) \n";
+	sleep(1);
 }
+#disconnect from PDE OSC server
+#$client->send(['/server/disconnect']);
