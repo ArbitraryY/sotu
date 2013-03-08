b938dc68aa1608c2ea6f82de297432af228a542e
ArbitraryY <pi@raspberrypi.(none)>
2013-03-07 23:22:29 -0800

    Updated perl OSC for rpi

--- perlOSC/osc_test/OSCserver.pl
+++ perlOSC/osc_test/OSCserver.pl
@@ -1,19 +1,34 @@
-#!C:\xampp\perl\bin\perl
+#!/usr/bin/perl
 
 use strict;
-use lib 'C:\xampp\perl\lib';
-#use warnings;
 use Net::OpenSoundControl::Server;
 my $port = 4567;
 
 use Data::Dumper qw(Dumper);
 
-sub dumpmsg {
-    print "[$_[0]] ", Dumper $_[1];
+sub ledAction {
+    my $oscMsgRef  = $_[1]; #assign OSC msg reference to var
+    my $oscAddress = "@{ $oscMsgRef }[0]";
+    my $oscType    = "@{ $oscMsgRef }[1]";
+    my $oscValue   = "@{ $oscMsgRef }[2]";
+    #print Dumper $oscMsgRef;
+    print "Address:" . "$oscAddress\n" ;
+    print "Type:" . "$oscType\n" ;
+    print "Value:" . "$oscValue\n\n";
+   
+    #parse color from OSC address
+    my $oscAddress =~ m/ard\/(\w+)/;
+    print "Color: " . $1 . "\n";
+    #hash of colors and corresponding GPIO on RPi
+    my %ledGPIO = (
+		"red"   => "5", #GPIO 23
+		"green" => "2", #     18
+		"blue"  => "6", #     24
+	       );    
 }
 
 my $server =
-    Net::OpenSoundControl::Server->new(Port => $port, Handler => \&dumpmsg)
+    Net::OpenSoundControl::Server->new(Port => $port, Handler => \&ledAction)
     or die "Could not start server: $@\n";
 
 print "[OSC Server] Receiving messages on port $port\n";
