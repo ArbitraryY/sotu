ce85fb56f0234228cfdd5d6802bc90a27b3ace69
ArbitraryY <pi@raspberrypi.(none)>
2013-03-11 20:45:33 -0700

    Added code to caluculate the distance based on high/low values of the sensor.  Use speed of sound to calculate the distance in both cm and in. Take 3 readings then avg them out for more accurate readings

--- platoon/rangeSensor.py
+++ platoon/rangeSensor.py
@@ -9,13 +9,40 @@ import time as time
 GPIO.setmode(GPIO.BCM)
  
 # Define GPIO to use on Pi
-GPIO_PIR = 22;
- 
-print "PIR Module Test (CTRL-C to exit)"
+GPIO_PWM =22; 
  
 # Set pin as input
-#GPIO.setup(GPIO_PIR,GPIO.IN)      # Echo
+GPIO.setup(GPIO_PWM,GPIO.IN)      # Echo
  
+def measure():
+  # This function measures a distance
+  
+  start = time.time()
+  while GPIO.input(GPIO_PWM)==0:
+    start = time.time()
+  
+  while GPIO.input(GPIO_PWM)==1:
+    stop = time.time()
+
+  elapsed = stop-start
+  #distance = (elapsed * 34300)/2 #speed of sound in cm/sec
+  distance = (elapsed * 13512)/2 #speed of sound in in/sec
+
+  return distance
+
+def measureAvg():
+  # This function takes 3 measurements and
+  # returns the average.
+
+  distance1=measure()
+  time.sleep(0.1)
+  distance2=measure()
+  time.sleep(0.1)
+  distance3=measure()
+  distance = distance1 + distance2 + distance3
+  distance = distance / 3
+  return distance
+
 try:
  
   print "Waiting for sensor to settle ..."
@@ -24,10 +51,13 @@ try:
   while True :
  
     # Read PIR state
-    Current_State = GPIO.input(GPIO_PIR)
- 
+    Current_State = GPIO.input(GPIO_PWM)
     print Current_State 
+    #Call distance Avg function
+    distance = measure()
+    print "Distance : %.1f" % distance
     time.sleep(1)
+
 except KeyboardInterrupt:
   print "  Quit"
   # Reset GPIO settings
