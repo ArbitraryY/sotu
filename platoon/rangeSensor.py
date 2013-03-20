d92a4dead51bf24ff5da9934e14e15fb9c390030
ArbitraryY <syntax@arbitraryy.com>
2013-03-20 02:08:37 -0700

    Updated code with multiple range sensor calculations to exit loops when user exits range.  The unreliable range sensor readings can sometimes cause erratic behavior of the LEDs.  Really need to improve the reliability of the range sensor readings

--- platoon/rangeSensor.py
+++ platoon/rangeSensor.py
@@ -13,8 +13,8 @@ import os
 # Define GPIO to use for range Sensor
 GPIO_RS =4; 
 #define the GPIO pins, r = 5, g = 2, b = 6
-GPIO_PINS_LED_1 = [1,4,6]
-#GPIO_PINS_LED_2 = [5,2,6]
+GPIO_PINS_LED_1 = [2,5,7]
+GPIO_PINS_LED_2 = [1,4,6]
 
 #set mode to BCM so PWM
 GPIO.setmode(GPIO.BCM)
@@ -26,7 +26,7 @@ GPIO.setup(GPIO_RS,GPIO.IN)
 global FADESPEED
 global STEP
 
-FADESPEED = 0.1 #Increase to slow down
+FADESPEED = 0.01 #Increase to slow down
 STEP = 0.03
 
 # define LED RGB colors
@@ -69,11 +69,13 @@ def measureAvg():
   	distance = distance / 3
   	return distance
 
-def fadeLED( gpio, startVal, stopVal ):
-        """This function takes 3 values
-        gpio - value of the RPi GPIO pin that will be updated
-        startVal - RGB value that the fade will start from
-        stopVal - RGB value that the fade will stop at
+def fadeLED( gpio, startVal, stopVal, lower, upper ):
+        """This function takes the following arguements
+        gpio: value of the RPi GPIO pin that will be updated
+        startVal: RGB value that the fade will start from
+        stopVal: RGB value that the fade will stop at
+	lower: Lower bound of the current range
+	upper: Upper bound of the current range 
 
         If the stop value is higher need to increment to get there.
         If the stop value is lower need to decrement to get there
@@ -82,22 +84,45 @@ def fadeLED( gpio, startVal, stopVal ):
         RGBstartVal = startVal / 255
         RGBstopVal = stopVal / 255
         #debug
-        print RGBstartVal, startVal, RGBstopVal, stopVal;
+        #print RGBstartVal, startVal, RGBstopVal, stopVal;
         #set the current LED values to the start value
+	
         currentVal = RGBstartVal
         if RGBstartVal < RGBstopVal:
                 while currentVal < RGBstopVal:
                         os.system("echo \"{0}={1}\" > /dev/pi-blaster" .format(gpio,currentVal))
                         currentVal = currentVal + STEP;
                         time.sleep(FADESPEED)
-                        print currentVal
+    			#take a distance measurment and check if out of range
+			distance = measure()
+			print "distance in loop addition loop: %.3f" % distance
+			if distance < lower or distance > upper:
+				allOff();
+				break
+                        #print currentVal
         elif RGBstartVal > RGBstopVal:
                  while currentVal > RGBstopVal:
                         os.system("echo \"{0}={1}\" > /dev/pi-blaster" .format(gpio,currentVal))
                         currentVal = currentVal - STEP;
                         time.sleep(FADESPEED)
-                        print currentVal
+    			#take a distance measurment
+			distance = measure()
+			print "distance in loop subtracting loop: %.3f" % distance
+			if distance < lower or distance > upper:
+				allOff();
+				break
+                        #print currentVal
         return;
+
+def allOff():
+	"""This function turns all the LEDs off
+	"""
+	for gpioVal in GPIO_PINS_LED_1:
+        	os.system("echo \"{0}=0\" > /dev/pi-blaster" .format(gpioVal))
+	for gpioVal in GPIO_PINS_LED_2:
+        	os.system("echo \"{0}=0\" > /dev/pi-blaster" .format(gpioVal))
+	
+	
 
 #-------------------End Funcs -----------------------------------------
 
@@ -114,30 +139,66 @@ try:
 	distance = measure()
     	print "Distance : %.1f" % distance
 	#check which range we are in
-	if distance >= ranges[0] and distance <= ranges[1]:
+	#if distance >= ranges[0] and distance <= ranges[1]:
+	i = 0
+	j = 0
+	while distance >= ranges[0] and distance <= ranges[1]:
 		print "------------Range 1-----------"
 		#set LED to initial value
 		
+		#determine the number of colors in each array	
+		LED_1_COLORS_LENGTH = len(RNG_1_LED_1)
+		LED_2_COLORS_LENGTH = len(RNG_1_LED_2)
+		#measure distance again and exit loop if out of range
+		distance = measure()
+		print "distance in loop: %.3f" % distance
+		if distance < ranges[0] or distance > ranges[1]:
+			allOff()
+			break	
+		#fade red LEDs
+		fadeLED(GPIO_PINS_LED_1[0],RNG_1_LED_1[i][0],RNG_1_LED_1[i+1][0],ranges[0],ranges[1]);
+		fadeLED(GPIO_PINS_LED_2[0],RNG_1_LED_2[j][0],RNG_1_LED_2[j+1][0],ranges[0],ranges[1]);	
+		#fade green LEDs
+		fadeLED(GPIO_PINS_LED_1[1],RNG_1_LED_1[i][1],RNG_1_LED_1[i+1][1],ranges[0],ranges[1]);
+		fadeLED(GPIO_PINS_LED_2[1],RNG_1_LED_2[j][1],RNG_1_LED_2[j+1][1],ranges[0],ranges[1]);	
+		#fade blue LEDs
+		fadeLED(GPIO_PINS_LED_1[2],RNG_1_LED_1[i][2],RNG_1_LED_1[i+1][2],ranges[0],ranges[1]);
+		fadeLED(GPIO_PINS_LED_2[2],RNG_1_LED_2[j][2],RNG_1_LED_2[j+1][2],ranges[0],ranges[1]);	
+		i = i + 1
+		j = j + 1
+		#check if array dimensions have gone out of range
+		if i > LED_1_COLORS_LENGTH - 2:
+			i = 0
+		if j > LED_2_COLORS_LENGTH - 2:
+			j = 0
+		print i , j
+		print LED_1_COLORS_LENGTH, LED_2_COLORS_LENGTH
+		'''
 		for c in range(len(RNG_1_LED_1)-1):
         		#loop within each subarray
         		for a in range(len(RNG_1_LED_1[c])):
                 		#pass GPIO pin to which manipulate
-                		fadeLED(GPIO_PINS_LED_1[a],RNG_1_LED_1[c][a],RNG_1_LED_1[c+1][a]);
-        		print "--------------------"
+               			fadeLED(GPIO_PINS_LED_1[a],RNG_1_LED_1[c][a],RNG_1_LED_1[c+1][a]);
+	
+        	print "--------------------"
+		'''
 
+	'''
 	elif distance >= ranges[2] and distance <= ranges[3]:
 		print "------------Range 2-----------"
 	elif distance >= ranges[4] and distance <= ranges[5]:
 		print "------------Range 3-----------"
-	else:
+	else
 		print "not in range"
-
+	'''
 
     # time between measurments
     	time.sleep(1)
 
 except KeyboardInterrupt:
 	for gpioVal in GPIO_PINS_LED_1:
+        	os.system("echo \"{0}=0\" > /dev/pi-blaster" .format(gpioVal))
+	for gpioVal in GPIO_PINS_LED_2:
         	os.system("echo \"{0}=0\" > /dev/pi-blaster" .format(gpioVal))
 
 		print "  Quit"
