e38372a36bd1593362d482596dac82f4da6e27e7
ArbitraryY <syntax@arbitraryy.com>
2013-03-25 00:52:47 -0700

    Updated reangeSensor arduino test sketch to include analog reads

--- platoon/tests/rangeSensor/rangeSensor.ino
+++ platoon/tests/rangeSensor/rangeSensor.ino
@@ -1,14 +1,20 @@
 const int pwPin = 8;
+const int anPin = 0;
 double pulse, d; 
+float anVolt;
 
 void setup()  { 
   Serial.begin(19200);  
 } 
 void loop(){
   pinMode(pwPin, INPUT);
+  pinMode(anPin, INPUT);
+  
+  anVolt = analogRead(anPin);
   pulse = pulseIn(pwPin, HIGH);
   d = pulse/147;
   //cm = inches * 2.54;
-  Serial.println(d);
-  delay(2);
+  //Serial.println(d);
+  Serial.println(anVolt);
+  delay(5);
 }
