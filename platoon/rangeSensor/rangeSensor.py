3cd15db74b3db6af03ac19c8eb814de103b6e993
ArbitraryY <syntax@arbitraryy.com>
2013-04-02 01:27:25 -0700

    * Removed range old range sensor measurement functions that were moved to rsDistance.py module * Removed extraneous code from rsDistance module * Updated arduino test sketch
    * Fixed #26 by adding a return value from the fadeLed() function to alert other calls to fadeLED() that user has exited range

--- platoon/rangeSensor/rangeSensor.py
+++ platoon/rangeSensor/rangeSensor.py
@@ -9,6 +9,7 @@ from __future__ import division #needed for division of integers --> floats
 import RPi.GPIO as GPIO
 import time
 import os
+import rsDistance
 
 # Define GPIO to use for range Sensor
 GPIO_RS =4; 
@@ -46,63 +47,7 @@ RNG_1_LED_2 = [ [150,44,37],[32,69,97],[121,65,137],[121,65,137],[238,68,79] ]
 #define ranges
 ranges=[10.0,16.0,17.0,47.0,48.0,70.0];
 
-#This variable holds the global type of average when taking range measurements
-#Options are aMean (arithmetic Mean), median, iquart (interquartile average)
-global AVGTYPE 
-AVGTYPE = 'median'
-
 #----------------------Funcs-----------------------------------------
-def measure():
-	"""This function measures the distance the echo pulse has traveled.
-	i.e. the time it takes for the sensor to go from 0 --> 1 or 1 --> 0)
-	"""
- 	start = time.time()
-  	while GPIO.input(GPIO_RS)==0:
-    		start = time.time()
-	while GPIO.input(GPIO_RS)==1:
-    		stop = time.time()
-	elapsedTime = stop-start
-  	#distance = (elapsed * 34300)/2 #speed of sound in cm/sec
-  	distance = (elapsedTime * 13512)/2 #speed of sound in in/sec
-	return distance
-
-def measureAvg(avgType):
-	"""Arguements:
-	AVGTYPE - Which average algorithm to use (aMean|median|iquart)
-	"""
-	#Number of measurements to take.  Use a multiple of 4 if using the
-	#interquartile mean
-	numMeasures = 3;
-	dataPoints = []
-	#take numMeasures # of measurements and put into array for avg calculations
-	for num in range(0,numMeasures):
-		distance=measure()
-		time.sleep(0.01)
-		dataPoints.append(distance)
-	avg = 0
-	#calculate the Arithmetic Mean
-	if avgType == 'aMean':
-		for i in range(0,numMeasures):
-			avg += dataPoints[i]
-			print avg
-		avg = avg / numMeasures 	
-	#calculate the Median
-	elif avgType == 'median':
-		#calculate the median
-		theValues = sorted(dataPoints)
-                if len(theValues) % 2 == 1:
-			print int((len(theValues)+1)/2-1)
-                        avg = theValues[int((len(theValues)+1)/2-1)]
-                else:
-                        lower = theValues[int(len(theValues)/2-1)]
-                        upper = theValues[int(len(theValues)/2)]
-                        avg = (float(lower + upper)) / 2
-	elif avgType == 'iquart':
-		print "iquart calc"	
-  	else:
-		print "not a valid Averaging option"
-	return avg
-
 def fadeLED( gpio, startVal, stopVal, lower, upper ):
         """This function takes the following arguements
         gpio: value of the RPi GPIO pin that will be updated
@@ -117,10 +62,12 @@ def fadeLED( gpio, startVal, stopVal, lower, upper ):
         #convert passed values into usable format for pi-blaster (i.e 0 - 1)
         RGBstartVal = startVal / 255
         RGBstopVal = stopVal / 255
+	#this variable will be returned as a check whether or not someone is in/out
+	# of range
+	rangeVal = 1;	
         #debug
         #print RGBstartVal, startVal, RGBstopVal, stopVal;
         #set the current LED values to the start value
-	
         currentVal = RGBstartVal
         if RGBstartVal < RGBstopVal:
                 while currentVal < RGBstopVal:
@@ -128,11 +75,13 @@ def fadeLED( gpio, startVal, stopVal, lower, upper ):
                         currentVal = currentVal + STEP;
                         time.sleep(FADESPEED)
     			#take a distance measurment and check if out of range
-			distance = measureAvg(AVGTYPE)
-			#distance = measure()
+			distance = rsDistance.measureAvg()
+			#distance = rsDistance.arduinoMeasure()
 			print "distance in loop addition loop: %.3f" % distance
 			if distance < lower or distance > upper:
+				#fadeOut();
 				allOff();
+				rangeVal = 0;
 				break
                         #print currentVal
         elif RGBstartVal > RGBstopVal:
@@ -141,14 +90,15 @@ def fadeLED( gpio, startVal, stopVal, lower, upper ):
                         currentVal = currentVal - STEP;
                         time.sleep(FADESPEED)
     			#take a distance measurment
-			distance = measureAvg(AVGTYPE)
-			#distance = measure()
+			distance = rsDistance.measureAvg()
+			#distance = rsDistance.arduinoMeasure()
 			print "distance in loop subtracting loop: %.3f" % distance
 			if distance < lower or distance > upper:
+				#fadeOut();
 				allOff();
+				rangeVal = 0;
 				break
-                        #print currentVal
-        return;
+        return rangeVal;
 
 def setColor(ledStripNum,R,G,B):
 	"""
@@ -193,7 +143,8 @@ try:
     	Current_State = GPIO.input(GPIO_RS)
     	print Current_State 
     	#take a distance measurment
-	distance = measureAvg(AVGTYPE)
+	distance = rsDistance.measureAvg()
+	#distance = rsDistance.arduinoMeasure()
     	print "Average Distance : %.1f" % distance
 	#check which range we are in
 	#if distance >= ranges[0] and distance <= ranges[1]:
@@ -206,30 +157,34 @@ try:
 		setColor(2,RNG_1_LED_2[0][0],RNG_1_LED_2[0][1],RNG_1_LED_2[0][2])
 	#elif distance >= ranges[2] and distance <= ranges[3]:
 	#elif distance >= ranges[4] and distance <= ranges[5]:
-	
+	rangeVal = 1;	
 	while distance >= ranges[0] and distance <= ranges[1]:
 		print "------------Range 1-----------"
-		#set LED to initial value
-		
-			
 		#determine the number of colors in each array	
 		LED_1_COLORS_LENGTH = len(RNG_1_LED_1)
 		LED_2_COLORS_LENGTH = len(RNG_1_LED_2)
 		#measure distance again and exit loop if out of range
-		distance = measureAvg(AVGTYPE)
+		distance = rsDistance.measureAvg()
+		#distance = rsDistance.arduinoMeasure()
 		print "distance in loop: %.3f" % distance
 		if distance < ranges[0] or distance > ranges[1]:
 			allOff()
 			break	
 		#fade red LEDs
-		fadeLED(GPIO_PINS_LED_1[0],RNG_1_LED_1[i][0],RNG_1_LED_1[i+1][0],ranges[0],ranges[1]);
-		fadeLED(GPIO_PINS_LED_2[0],RNG_1_LED_2[j][0],RNG_1_LED_2[j+1][0],ranges[0],ranges[1]);	
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_1[0],RNG_1_LED_1[i][0],RNG_1_LED_1[i+1][0],ranges[0],ranges[1]);
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_2[0],RNG_1_LED_2[j][0],RNG_1_LED_2[j+1][0],ranges[0],ranges[1]);	
 		#fade green LEDs
-		fadeLED(GPIO_PINS_LED_1[1],RNG_1_LED_1[i][1],RNG_1_LED_1[i+1][1],ranges[0],ranges[1]);
-		fadeLED(GPIO_PINS_LED_2[1],RNG_1_LED_2[j][1],RNG_1_LED_2[j+1][1],ranges[0],ranges[1]);	
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_1[1],RNG_1_LED_1[i][1],RNG_1_LED_1[i+1][1],ranges[0],ranges[1]);
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_2[1],RNG_1_LED_2[j][1],RNG_1_LED_2[j+1][1],ranges[0],ranges[1]);	
 		#fade blue LEDs
-		fadeLED(GPIO_PINS_LED_1[2],RNG_1_LED_1[i][2],RNG_1_LED_1[i+1][2],ranges[0],ranges[1]);
-		fadeLED(GPIO_PINS_LED_2[2],RNG_1_LED_2[j][2],RNG_1_LED_2[j+1][2],ranges[0],ranges[1]);	
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_1[2],RNG_1_LED_1[i][2],RNG_1_LED_1[i+1][2],ranges[0],ranges[1]);
+		if(rangeVal):
+			rangeVal = fadeLED(GPIO_PINS_LED_2[2],RNG_1_LED_2[j][2],RNG_1_LED_2[j+1][2],ranges[0],ranges[1]);	
 		i = i + 1
 		j = j + 1
 		#check if array dimensions have gone out of range
@@ -240,15 +195,6 @@ try:
 			j = 0
 		print i , j
 		print LED_1_COLORS_LENGTH, LED_2_COLORS_LENGTH
-		'''
-		for c in range(len(RNG_1_LED_1)-1):
-        		#loop within each subarray
-        		for a in range(len(RNG_1_LED_1[c])):
-                		#pass GPIO pin to which manipulate
-               			fadeLED(GPIO_PINS_LED_1[a],RNG_1_LED_1[c][a],RNG_1_LED_1[c+1][a]);
-	
-        	print "--------------------"
-		'''
 
 	'''
 	elif distance >= ranges[2] and distance <= ranges[3]:
