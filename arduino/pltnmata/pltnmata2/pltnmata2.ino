946fe915017510d863a132fba8c387a126b2c401
ArbitraryY <syntax@arbitraryy.com>
2013-04-23 12:43:25 -0700

    Updated painting title to be less than 32 chars

--- arduino/pltnmata/pltnmata2/pltnmata2.ino
+++ arduino/pltnmata/pltnmata2/pltnmata2.ino
@@ -599,9 +599,9 @@ void setup()
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Energy Isn't Cre");
+  lcd.print("Energy Not Creat");
   lcd.setCursor(0, 1);
-  lcd.print("ated or Destroyed");
+  lcd.print("ed or Destroyed ");
 }
 
 /*==============================================================================
