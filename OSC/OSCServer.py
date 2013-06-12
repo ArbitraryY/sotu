6049596d0272f062a0aa9194314ef4aca39d0518
ArbitraryY <syntax@arbitraryy.com>
2013-06-11 23:09:36 -0700

    Added callback functions for each OSC address

--- OSC/OSCServer.py
+++ OSC/OSCServer.py
@@ -2,28 +2,44 @@
 
 from OSC import OSCServer
 import sys
-
 sys.path.append("/home/nick/platoon/rangeSensor")
-print sys.path
-import LED
-
+#import LED
+import re
 
 OSCPort = 4567
 OSCIP   = "0.0.0.0"
 
 oscSrv = OSCServer((OSCIP,OSCPort))
 
-def red1(path, tags, args, source):
-	print path 
+def ledActn(path, tags, args, source):
+	print path
+	print args
+	m = re.match('\/led\/(\w+)', path)
+	print m.group(0)
+	# = split(path)
+
+def rpiActn(path, tags, args, source):
+	print path
+	print args
+
+def pltnActn(path, tags, args, source):
+	print path
+	print args
+
+def srvcActn(path, tags, args, source):
+	print path
+	print args
+
+#Message Handlers and Callback functions
+oscSrv.addMsgHandler("/led",ledActn)
+oscSrv.addMsgHandler("/srvc",srvcActn)
+oscSrv.addMsgHandler("/pltn",pltnActn)
+oscSrv.addMsgHandler("/rpi",rpiActn)
 
 print "listening on port" 
 print OSCPort
+
 while True:
 	oscSrv.handle_request()
-	oscSrv.addMsgHandler("/led/red1",red1)
 
-	
-
-
-
-oscSrv.server_close()
+oscSrv.close()
