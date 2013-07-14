2e1277b7f07d91d5fe7e39cf33b12a8389b255d3
ArbitraryY <syntax@arbitraryy.com>
2013-07-14 13:48:48 -0700

    * Better module documentation

--- Modules/rsDistance/rsDistance.py
+++ Modules/rsDistance/rsDistance.py
@@ -3,7 +3,8 @@
 
 	This module calculates range sensor measurements.  It uses an echo pulse
 	algorithm to calculate the range from the voltage readings of the range sensor
-	connected to the Arduino
+	connected to the Arduino.  It uses pyfirmata to communicate with the Arduino over 
+	serial USB.  
 """
 
 import pyfirmata
@@ -21,16 +22,20 @@ it.start()
 #Options are aMean (arithmetic Mean), median
 global avgType
 avgType = 'median'
+#the number of measurements to take for averaging
 global numMeasures
 numMeasures = 10
 
-# set up pins
-pin0=board.get_pin('a:0:i')             # A0 Input      (LM35)
+# Set up the RPi to read the analog values from pin 0 on the Arduino
+pin0=board.get_pin('a:0:i') 
 
 def arduinoMeasure():
-	'''This function measures the distance when the range sensor is connected to pin A0 
-	on the Arduino
-	'''
+	"""(RS connected to Arduino) This function measures the distance of the nearest object
+		in the range of the range sensor
+		Input: None
+		Return:
+			distance: The distance in inches an object is from the range sensor
+	"""
 	while pin0.read() is None:
         	print "passing"
         	pass
@@ -38,9 +43,12 @@ def arduinoMeasure():
         return distance
 
 def piMeasure(GPIO_RS):
-        """This function measures the distance the echo pulse has traveled.
-        i.e. the time it takes for the sensor to go from 0 --> 1 or 1 --> 0)
-		GPIO_RS
+        """(RS connected to RPi) this function will measure the distance the echo pulse has 
+        	traveled. i.e. the time it takes for the sensor to go from 0 --> 1 or 1 --> 0) 
+        	Input: 
+           		GPIO_RS: The GPIO pin that the range sensor is connected to 
+       		Return: 
+           		distance: The distance an object is from the range sensor
         """
         start = time.time()
         while GPIO.input(GPIO_RS)==0:
@@ -53,26 +61,28 @@ def piMeasure(GPIO_RS):
         return distance
 
 def measureAvg():
-        """Used to smooth out the range sensor measurements. X number of measurements
-        are taken and averaged out using either Arithmetic Mean (aMean) or median.
-        Set the value AVGTYPE accordingly
+		"""This function takes "numMeasures" number of range sensor measurements and smoothes them 
+			out using averaging function (AVGTYPE) using either Arithmetic Mean (aMean) or median.
+        	Input: None
+        	Return:
+				avg: The average value of "numMeasures" range sensor measurements		
         """
-        #Number of measurements to take
-        dataPoints = []
-        #take numMeasures # of measurements and put into array for avg calculations
-        for num in range(0,numMeasures):
+		#Number of measurements to take
+		dataPoints = []
+		#take numMeasures # of measurements and put into array for avg calculations
+		for num in range(0,numMeasures):
         	#measure the range numMeasures times and put them into an array
-                distance=arduinoMeasure()
-                time.sleep(0.01)
-                dataPoints.append(distance)
-        avg = 0
-        #calculate the Arithmetic Mean
-        if avgType == 'aMean':
-        	avg = np.mean(dataPoints)
-        #calculate the Median
-        elif avgType == 'median':
-        	avg = np.median(dataPoints)
-        else:
-                print "not a valid Averaging option"
-        return avg
+			distance=arduinoMeasure()
+			time.sleep(0.01)
+			dataPoints.append(distance)
+		avg = 0
+		#calculate the Arithmetic Mean
+		if avgType == 'aMean':
+			avg = np.mean(dataPoints)
+		#calculate the Median
+		elif avgType == 'median':
+			avg = np.median(dataPoints)
+		else:
+			print "not a valid Averaging option"
+		return avg
 
