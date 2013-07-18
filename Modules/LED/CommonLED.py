7761136e1d1e3599631f57abf54ed6e7f0239ad3
ArbitraryY <syntax@arbitraryy.com>
2013-07-17 23:41:08 -0700

    * Removed functions that were moved to effects module

--- Modules/LED/CommonLED.py
+++ Modules/LED/CommonLED.py
@@ -17,12 +17,11 @@ class CommonLED():
 	def __init__(self):
 		self.pg              = pltnGpio.pltnGpio() 
 		self.pb              = Pblstr.Pblstr()
-		self.stepSize        = 0.05 #STEP Size for fading out 
 		
 	def analogToDigital(self,analogColors):
 		"""Converts RGB analog values to Digital for pi-blaster
 			Input: 
-				analogColors: 2D list of analog RGB valueslors) and converts them to digital.
+				analogColors: 2D list of analog RGB values) and converts them to digital.
 			Return: 2D list of digital RGB values (digitalColors)
 		"""
 		digitalColors = [[Decimal(x)/Decimal(255) for x in y] for y in analogColors]
@@ -58,41 +57,3 @@ class CommonLED():
         	"""
         	self.setColor(1,[0,0,0])
         	self.setColor(2,[0,0,0])
-	
-	def ledFlashFade(self, gpioPin, pinValue):
-		"""Turn an LED on then fade it out immediately
-			Input:
-				gpioPin: GPIO pin to turn on
-				pinValue: Value to set the pin to (per pi-blaster 0 - 1)
-			Return: None 
-		"""
-		self.fadeOutSinglePin(gpioPin, pinValue,self.stepSize)
-	
-	def flash(self,gpioPin,sleepTime):
-		"""Turn an LED on then off immediately
-			Input:
-				gpioPin: GPIO pin to turn on then off
-			Return: None
-		"""
-		self.setPinValue(gpioPin,1)
-		sleep(sleepTime)
-		self.setPinValue(gpioPin,0)
-			
-	def fadeOutSinglePin(self, gpioPinVal, currentVal, stepSize):
-		'''This function takes a GPIO pin value and a current LED color
-		   value and fades it out completely
-		   Input:
-		   		gpioPinVal: Pin to fade out
-		   		currentVal: Fade from this value to 0
-		   		stepSize: Size of value to step down from currentVal
-	   		Return: None 
-		'''
-		while currentVal > 0.00:
-			self.pb.write(gpioPinVal,currentVal)
-			currentVal -= stepSize;
-			#set to zero once it gets negative
-			if currentVal < 0.00:
-				self.pb.write(gpioPinVal,0)
-				#set current colors to zero before exiting loop
-				currentVal = 0.00
-			print "%0.2f" % currentVal
